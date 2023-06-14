bee52ea99fab6ed6d3ac27cce764a6da4715678d
seerzhang <398893736@qq.com>
2023-06-14 12:57:05 +0800

    '20230614_06'

--- js/base.js
+++ js/base.js
@@ -1,1 +1,25 @@
+window.onload = function(){
+
+    function iEsc(){ return false; }
+
+    function iRec(){ return true; }
+
+    function DisableKeys() {
+        if(event.ctrlKey || event.shiftKey || event.altKey)  {
+            window.event.returnValue=false;
+            iEsc();
+        }
+    }
+
+    document.ondragstart=iEsc;
+    document.onkeydown=DisableKeys;
+    document.oncontextmenu=iEsc;
+
+    if (typeof document.onselectstart !="undefined"){
+        document.onselectstart=iEsc;
+    } else {
+        document.onmousedown=iEsc;
+        document.onmouseup=iRec;
+    }
+}
 debugger;
